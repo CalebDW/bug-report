8b00c8810ffeef67ce4c520ce4f03925ffcbc042
Caleb White <cwhite@eaglemetal.com>
2022-10-24 07:51:50 -0500

    Bug Report | Create failing attribute

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,21 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    protected $appends = [
+        'firstName',
+        'initials',
+    ];
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +48,23 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    // This accessor CAN be appended
+    protected function initials(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => preg_replace('/[^A-Z]/', '', $this->name),
+        );
+    }
+
+    // This accessor CANNOT be appended, calling `toArray()`
+    // on a model tries to call the old style
+    // `getFirstNameAttribute()` method
+    protected function firstName(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => Str::before($this->name, ' '),
+        );
+    }
 }
